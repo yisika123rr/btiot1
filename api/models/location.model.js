c7e75c0abb563185a597981d31e7ffa29b00fda4
yisika123rr <hieukul26@gmail.com>
2025-09-27 22:49:15 +0700

    Thêm name, lat, long cho Location model

--- api/models/location.model.js
+++ api/models/location.model.js
@@ -1,17 +1,41 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
+
+// const locationSchema = new mongoose.Schema(
+//   {
+//     lat: {
+//       type: String,
+//       required: true,
+//     },
+//     long: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.model('Location', locationSchema);
+import mongoose from "mongoose";
 
 const locationSchema = new mongoose.Schema(
   {
-    lat: {
+    name: {
       type: String,
+      required: true,
+    },
+    lat: {
+      type: Number,
       required: true,
     },
     long: {
-      type: String,
+      type: Number,
       required: true,
-    },
+    },git add api/models/location.model.js
+git commit -m "Thêm name, lat, long cho Location model"
+git push origin main
+
   },
   { timestamps: true }
 );
 
-export default mongoose.model('Location', locationSchema);
+export default mongoose.model("Location", locationSchema);
