6e554247c24127c084a698f6374c39732431c934
yisika123rr <hieukul26@gmail.com>
2025-09-27 22:59:45 +0700

    Fix dấu phẩy và hoàn thiện Location model

--- api/models/location.model.js
+++ api/models/location.model.js
@@ -1,20 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const locationSchema = new mongoose.Schema(
-//   {
-//     lat: {
-//       type: String,
-//       required: true,
-//     },
-//     long: {
-//       type: String,
-//       required: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model('Location', locationSchema);
 import mongoose from "mongoose";
 
 const locationSchema = new mongoose.Schema(
@@ -29,11 +12,8 @@ const locationSchema = new mongoose.Schema(
     },
     long: {
       type: Number,
-      required: true,
-    },git add api/models/location.model.js
-git commit -m "Thêm name, lat, long cho Location model"
-git push origin main
-
+      required: true
+    }
   },
   { timestamps: true }
 );
